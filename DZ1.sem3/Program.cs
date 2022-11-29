a49d0f3ad962d6addd7f12db8812c2947065db83
yuliya-gri <yuliya.grigoreva.1986@list.ru>
2022-11-29 14:55:18 +0400

    commit DZ1

---
+++ DZ1.sem3/Program.cs
@@ -0,0 +1,23 @@
+﻿double input( string x, string y)
+{
+    Console.WriteLine($"Введите {x} координату точки {y}");
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+try
+{
+    double xA = input("x", "A");
+    double yA = input("y", "A");
+    double zA = input("z", "A");
+    double xB = input("x", "B");
+    double yB = input("y", "B");
+    double zB = input("z", "B");
+   
+    double result = Math.Sqrt(Math.Pow((xB-xA), 2) + Math.Pow((yB-yA), 2) + Math.Pow((zB-zA), 2));
+    Console.WriteLine(("Расстояние между точками:") + Math.Round(result, 2));
+
+}
+catch 
+{
+    Console.WriteLine("ошибка выполнения");
+}
