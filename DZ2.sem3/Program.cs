182e442749715250fe4fb4888a7a4e42ae3819a4
yuliya-gri <yuliya.grigoreva.1986@list.ru>
2022-12-01 11:02:03 +0400

    commit DZ2

---
+++ DZ2.sem3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая на вход принимает пятизначное число и проверяет,является ли оно палиндромом
+
+Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = num/10000;
+int num2 = num%10000/1000;
+int num4 = num%100/10;
+int num5 = num%10;
+
+if (num1 == num5 && num2 == num4) 
+{
+    Console.WriteLine("Данное число палиндром");
+}
+
+else
+{
+    
+    Console.WriteLine("Данное число не палиндром");
+}
