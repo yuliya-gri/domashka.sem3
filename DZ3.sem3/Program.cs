6f1c5fd2ba8d1919d251c47c80fdda3c0cc632e0
yuliya-gri <yuliya.grigoreva.1986@list.ru>
2022-12-02 12:28:09 +0400

    commit DZ3

---
+++ DZ3.sem3/Program.cs
@@ -0,0 +1,15 @@
+﻿//написать программу ,которая принимает на ввод число N и выдает таблицу кубов
+Console.WriteLine("Напишите целое число");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Таблица кубов числа:");
+if(N < 0) N = -N;
+else 
+if(N == 0)
+{
+    Console.WriteLine("0");
+}
+for(int i =1; i <= N; i++)
+{
+    int InCube = (i*i*i);
+    Console.WriteLine($"{InCube}");
+}
